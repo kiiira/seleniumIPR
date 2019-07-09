b2f4bd6f0fac4027ad6c4cb8d33cd2d12d3d3106
Kira Zyubanova <zyubanovakira@gmail.com>
2019-07-09 13:39:20 +0300

    logger configured

--- src/test/java/Run/StepDefinitions.java
+++ src/test/java/Run/StepDefinitions.java
@@ -4,14 +4,15 @@ import PageObject.*;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import io.qameta.allure.Step;
+import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+
+import static PageObject.LetterPage.randomBodyText;
 
 public class StepDefinitions {
 
 
     //TODO: add different words to string builder
-    //TODO: add logger appenders
 
     private LoginPage loginPage = new LoginPage();
     private MainPage mainPage = new MainPage();
@@ -19,7 +20,7 @@ public class StepDefinitions {
     private LetterPage letterPage = new LetterPage();
     private BasicPage basicPage = new MainPage();
 
-    private final Logger log = Logger.getLogger(Logger.class);
+    static final Logger LOG = LogManager.getLogger(StepDefinitions.class.getName());
 
     private String testHost = "https://www.google.com/gmail/";
 
@@ -27,125 +28,132 @@ public class StepDefinitions {
     @Given("test host open")
     public void openHost() {
         basicPage.navigateTo(this.testHost);
-        log.info("Test host opened");
+        LOG.info("Test host opened");
     }
 
 
     @And("^fill e-mail field with value \"([^\"]*)\"$")
     public void fillEMailFieldWithValue(String value) {
         loginPage.fillEmailFieldWithValue(value);
-        log.info(String.format("E-mail field filled with value %s", value));
+        LOG.info(String.format("E-mail field filled with value %s", value));
     }
 
 
     @And("^fill password field with value \"([^\"]*)\"$")
     public void fillPasswordFieldWithValue(String value) {
         loginPage.fillPasswordFieldWithValue(value);
-        log.info(String.format("Password field filled with value %s", value));
+        LOG.info(String.format("Password field filled with value %s", value));
     }
 
 
     @And("^press \"([^\"]*)\" button$")
     public void pressButton(String buttonLabel) {
         loginPage.pressButton(buttonLabel);
-        log.info("");
+        LOG.info(String.format("\"%s\" button pressed", buttonLabel));
     }
 
 
     @And("^click \"([^\"]*)\" div button$")
     public void clickDivButton(String divButtonLabel) {
         mailPage.clickDivButton(divButtonLabel);
-        log.info("");
+        LOG.info(String.format("\"%s\" div button pressed", divButtonLabel));
     }
 
 
     @And("^fill \"([^\"]*)\" addressee field with value \"([^\"]*)\"$")
     public void fillTextAreaFieldWithValue(String fieldLabel, String value) {
         letterPage.fillTextAreaElementWithValue(fieldLabel, value);
-        log.info("");
+        LOG.info(String.format("Text area \"%s\" filled with value \"%s\"", fieldLabel, value));
     }
 
 
     @And("^fill \"([^\"]*)\" subject field with random value$")
     public void fillInputFieldValue(String fieldLabel) {
         letterPage.fillInputElementWithRandomValue(fieldLabel);
-        log.info("");
+        LOG.info(String.format("Input fields filled with value \"%s\"", fieldLabel));
     }
 
 
     @And("^fill \"([^\"]*)\" text field with value \"([^\"]*)\"$")
     public void fillDivTextFieldWithRandomValue(String fieldLabel, String inputText) {
         letterPage.fillDivTextFieldWithValue(fieldLabel, inputText);
-        log.info("");
+        LOG.info(String.format("Div text field \"%s\" filled with value \"%s\"", fieldLabel, inputText));
     }
 
 
     @And("^click \"([^\"]*)\" button in letter title bar$")
     public void clickButtonInLetterTitleBar(String button) {
         letterPage.pressTitleBarButton(button);
-        log.info("");
+        LOG.info(String.format("\"%s\" button clicked in letter title bar", button));
     }
 
 
     @And("^go to \"([^\"]*)\" folder$")
     public void goToFolder(String folderLabel) {
         mailPage.openFolder(folderLabel);
-        log.info("");
+        LOG.info(String.format("Navigating to folder \"%s\"", folderLabel));
     }
 
 
     @And("^the letter has been created$")
     public void checkLetterCreated() {
         mailPage.checkLetterCreated();
-        log.info("");
+        LOG.info("Congratulation! The letter has been created");
     }
 
 
     @And("^open the template$")
     public void openTheTemplate() {
         mailPage.openTemplate();
-        log.info("");
+        LOG.info("Opening the newly created template");
     }
 
 
     @And("^the template has addressee field as \"([^\"]*)\"$")
     public void checkTemplateAddressee(String value) {
         letterPage.checkAddressee(value);
+        LOG.info(String.format("The template has expected addressee field \"%s\"", value));
     }
 
 
     @And("^the template has subject field as randomly generated text$")
     public void checkTemplateSubject() {
         letterPage.checkSubjectBox();
+        LOG.info(String.format("The template has subject field as randomly generated text: \"%s\"", randomBodyText));
     }
 
 
     @And("^the template has body field as \"([^\"]*)\"$")
     public void checkTemplateBody(String expectedLetterBody) {
         letterPage.checkLetterBody(expectedLetterBody);
+        LOG.info(String.format("The template has body field as \"%s\"", expectedLetterBody));
     }
 
 
     @And("^press \"([^\"]*)\" button in letter window$")
     public void pressButtonInLetterWindow(String buttonLabel) {
         letterPage.clickButtonInLetterWindow(buttonLabel);
+        LOG.info(String.format("Pressing \"%s\" button in letter window", buttonLabel));
     }
 
 
     @Then("^the template has been deleted$")
     public void theTemplateHasBeenDeleted() {
         mailPage.checkTemplateDeleted();
+        LOG.info("The template has been successfully deleted");
     }
 
 
     @And("^press account button$")
     public void pressAccountButton() {
         mainPage.clickAccountButton();
+        LOG.info("Pressing account button");
     }
 
 
     @And("^press \"([^\"]*)\" button in account popup$")
     public void pressButtonInAccountPopup(String buttonLabel) {
         mainPage.clickAccountActionButton(buttonLabel);
+        LOG.info(String.format("Pressing \"%s\" button in account popup", buttonLabel));
     }
 }
